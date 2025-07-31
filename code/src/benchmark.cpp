7a375bfa0f46369da021db40ddad2c6b125765e3
Douglas Wayne Potter <douglaspotter@gmail.com>
2025-08-01 01:03:04 +0200

    wip: benchmark my RCSP algo

--- code/src/benchmark.cpp
+++ code/src/benchmark.cpp
@@ -13,12 +13,12 @@
 // <https://www.gnu.org/licenses/>.
 //
 
+#include "convert.h"
 #include "example_graphs.h"
 
 #include <benchmark/benchmark.h>
 
 static void BM_BoostRCSP(benchmark::State &state) {
-
   int seed = 42;
   for (auto _ : state) {
     const perf_rcsp::State initial_state{};
@@ -35,11 +35,30 @@ static void BM_BoostRCSP(benchmark::State &state) {
   }
 }
 
+static void BM_RCSP(benchmark::State &state) {
+  int seed = 42;
+  for (auto _ : state) {
+    const perf_rcsp::State initial_state{};
+    state.PauseTiming();
+    perf_rcsp::SourceTargetBoostGraph s_t_g;
+    perf_rcsp::generate(static_cast<int>(state.range(0)), seed, s_t_g);
+    auto graph = convert_to_graph(s_t_g.graph);
+    ++seed;
+    state.ResumeTiming();
+    auto solutions = find_solutions(graph, s_t_g.source_vertex, s_t_g.target_vertex, initial_state);
+    // It is intended that the generated instance should have some solutions.
+    ASSERT_ALWAYS(!solutions.nondominated_end_states.empty());
+    // help prevent optimizing away find_solutions.
+    seed += static_cast<int>(solutions.nondominated_end_states.size());
+  }
+}
+
 static void customer_sites_counts(benchmark::internal::Benchmark *b) {
-  for (int customer_sites_count = 5; customer_sites_count <= 10; ++customer_sites_count) {
+  for (int customer_sites_count = 5; customer_sites_count <= 20; customer_sites_count += 2) {
     b->Args({customer_sites_count});
   }
 }
 
 BENCHMARK(BM_BoostRCSP)->Unit(benchmark::kMillisecond)->Apply(customer_sites_counts);
+BENCHMARK(BM_RCSP)->Unit(benchmark::kMillisecond)->Apply(customer_sites_counts);
 BENCHMARK_MAIN();
